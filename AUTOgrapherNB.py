20a390ef776a0e13ca773debae8ba7c364630604
Simon Osborne <simonwosborne@gmail.com>
2012-11-04 17:59:43 +0000

    INITIAL COMMIT

---
+++ AUTOgrapherNB.py
@@ -0,0 +1,141 @@
+#!/usr/bin/env python
+from matplotlib import pyplot as plt
+import pylab
+import numpy as np
+import bifDiag
+import parseB
+import parseS
+import parseC
+import parseD
+import re
+
+
+def plotb(r2, **kwargs):
+    colors=['b', 'g', 'r', 'c', 'm', 'y', 'k']
+    fig=plt.figure()
+    fig.clf()
+    ax=fig.add_subplot(111)
+
+    n1b=parseB.parseB(r2)
+    ap=n1b[0]
+    c=parseC.parseC(n1b[0]) 
+
+    if kwargs.get('xlim'):
+        ax.set_xlim(kwargs.get('xlim'))
+    if kwargs.get('ylim'):
+        ax.set_ylim(kwargs.get('ylim'))   
+    if kwargs.get('x'):
+        x=kwargs.get('x')
+        numx=re.sub('\D', '',x)
+        param=x.strip(numx)
+        if param=='U':
+            paramx='U('+numx+')'
+        if param=='P':
+                paramx='PAR('+numx+')'
+        if param=='L':
+            paramx='L2-NORM'
+        if param=='T':
+            paramx='PERIOD'
+    else:
+        paramx='PAR(1)'
+
+    if kwargs.get('y'):
+        y=kwargs.get('y')
+        numy=re.sub('\D', '',y)
+        param=y.strip(numy)
+        if param=='U':
+            paramy='U('+numy+')'
+        if param=='P':
+            paramy='PAR('+numy+')'
+        if param=='L':
+            paramy='L2-NORM'
+        if param=='T':
+            paramy='PERIOD'
+    else:
+        paramy='L2-NORM'
+
+    x=[]
+    y=[]
+    special=[]
+    Branch=0
+    BranchLast=0
+    if ap['PT']>0:
+        current=True
+        last=True
+    else:
+        current=False
+        last=False
+
+    try:
+        xvalue=c[paramx]
+    except KeyError:
+        xvalue=c['MAX U('+numx+')']
+    try:
+        yvalue=c[paramy]
+    except KeyError:
+        yvalue=c['MAX U('+numy+')']
+
+    if ap['TY name']!='No Label':
+        special.append([ap['TY name'],xvalue,yvalue])
+    
+    x.append(xvalue)
+    y.append(yvalue)
+    Bchange=False #branch change
+    Bchecklast=ap['BR']
+    Bcheckcurrent=Bchecklast
+    Schange=False #changed of stability
+    #also can have different calculations
+    for i in range(1,len(n1b)):
+        if Schange==True or Bchange==True or i==len(n1b)-1:
+            color=colors[Branch%7]
+            if Bchange==True:
+                Branch=BranchLast+1
+            if last==True:
+                ax.plot(x[0:len(x)-2],y[0:len(x)-2],color=color,ls='dotted')
+            else:
+                ax.plot(x[0:len(x)-2],y[0:len(x)-2],color=color,ls='solid')
+            x=[]
+            y=[]
+            ap=n1b[i]
+            if ap['PT']>0:
+                current=True
+            else:
+                current=False
+            last=current
+            Schange=False
+            Bchange=False
+        last=current
+        Bcheckcurrent=ap['BR']
+        ap=n1b[i]
+
+        c=parseC.parseC(n1b[i]) 
+        try:
+            xvalue=c[paramx]
+        except KeyError:
+            print paramx
+            xvalue=c['MAX U('+numx+')']
+        try:
+            yvalue=c[paramy]
+        except KeyError:
+            yvalue=c['MAX U('+numy+')']
+        x.append(xvalue)
+        y.append(yvalue)
+        if ap['TY name']!='No Label':
+            special.append([ap['TY name'],xvalue,yvalue])
+
+        if ap['PT']==1 or ap['PT']==-1 or Bchecklast!=Bcheckcurrent:
+            Bchange=True
+#this is a fudge because sometimes ap['PT'] returned NoneType
+        # this should detect when new line is plotted as first point is 1
+        if ap['PT']>0:
+            current=True
+        else:
+            current=False
+        if current!=last:
+            Schange=True
+        BranchLast=Branch
+        Bchecklast=Bcheckcurrent
+
+    for i,sp in enumerate(special):
+        ax.plot(sp[1],sp[2],ls='None',marker='x',color='k',markersize=3)
+        ax.text(sp[1],sp[2],sp[0])
